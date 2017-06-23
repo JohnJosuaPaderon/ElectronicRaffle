9c8c4b01e8e121486c94395909b7c3a15eb13436
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-23 17:18:12 +0800

    Added: ApplicantTaggingControl.xaml; ApplicantTaggingControl.xaml.cs; ApplicationTaggingViewModel.cs; RelayCommand.cs Updated: RegisterNewTeacherControl.xaml

---
+++ ElectronicRaffle.CommonUi/Controls/ApplicantTaggingControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElectronicRaffle.CommonUi.Controls
+{
+    /// <summary>
+    /// Interaction logic for ApplicantTaggingControl.xaml
+    /// </summary>
+    public partial class ApplicantTaggingControl : UserControl
+    {
+        public ApplicantTaggingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ElectronicRaffle.CommonUi/Controls/ApplicationTaggingViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ElectronicRaffle.CommonUi.Controls
+{
+    public class ApplicationTaggingViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public RelayCommand CancelCommand { get; } = new RelayCommand(Cancel);
+
+        private static void Cancel(object obj)
+        {
+            var command = (ICommand)DialogHost.CloseDialogCommand;
+            command.Execute(obj);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
