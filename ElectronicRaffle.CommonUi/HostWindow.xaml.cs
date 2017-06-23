6af2d7decb545543a2cccc224b33896d9644bd35
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-23 17:18:58 +0800

    Added: HostWindow.xaml; HostWindow.xaml.cs Updated: ElectronicRaffle.CommonUi.csproj

---
+++ ElectronicRaffle.CommonUi/HostWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace ElectronicRaffle.CommonUi
+{
+    /// <summary>
+    /// Interaction logic for HostWindow.xaml
+    /// </summary>
+    public partial class HostWindow : MetroWindow
+    {
+        public HostWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
